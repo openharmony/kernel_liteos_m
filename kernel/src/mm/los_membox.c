eac4a04153ef832992eed3f12571a779d2062b2c
YOUR_NAME <huangjieliang@huawei.com>
2021-05-14 14:49:17 +0800

    Fix memory leak check and interity error check. Delete redundant judgments.
    Close #I3P10Y and #I3QNLT
    
    Change-Id: I94ff34099bc39345ef5490f185f70c4b87bfd415

--- kernel/src/mm/los_membox.c
+++ kernel/src/mm/los_membox.c
@@ -130,10 +130,6 @@ UINT32 LOS_MemboxInit(VOID *pool, UINT32 poolSize, UINT32 blkSize)
 
     MEMBOX_LOCK(intSave);
     boxInfo->uwBlkSize = LOS_MEMBOX_ALIGNED(blkSize + OS_MEMBOX_NODE_HEAD_SIZE);
-    if (boxInfo->uwBlkSize == 0) {
-        MEMBOX_UNLOCK(intSave);
-        return LOS_NOK;
-    }
     boxInfo->uwBlkNum = (poolSize - sizeof(LOS_MEMBOX_INFO)) / boxInfo->uwBlkSize;
     boxInfo->uwBlkCnt = 0;
     if (boxInfo->uwBlkNum == 0) {
