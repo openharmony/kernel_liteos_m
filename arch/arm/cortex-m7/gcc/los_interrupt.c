05175c8bbaea22882f84f6f800eaa2c2c7b89f16
houpengfei <houpengfei@talkweb.com.cn>
2022-01-17 20:35:04 +0800

    fix:backtrace严重BUG
    Signed-off-by: houpengfei <houpengfei@talkweb.com.cn>

--- arch/arm/cortex-m7/gcc/los_interrupt.c
+++ arch/arm/cortex-m7/gcc/los_interrupt.c
@@ -368,6 +368,7 @@ STATIC VOID OsExcRegInfo(const ExcInfo *excInfo)
     PRINTK("xPSR      = 0x%x\n", excInfo->context->uwxPSR);
 }
 
+#if (LOSCFG_KERNEL_BACKTRACE == 1)
 STATIC VOID OsExcBackTraceInfo(const ExcInfo *excInfo)
 {
     UINTPTR LR[LOSCFG_BACKTRACE_DEPTH] = {0};
@@ -384,6 +385,7 @@ STATIC VOID OsExcBackTraceInfo(const ExcInfo *excInfo)
     }
     PRINTK("----- backtrace end -----\n");
 }
+#endif
 
 STATIC VOID OsExcMemPoolCheckInfo(VOID)
 {
@@ -433,7 +435,9 @@ STATIC VOID OsExcInfoDisplay(const ExcInfo *excInfo)
     OsExcTypeInfo(excInfo);
     OsExcCurTaskInfo(excInfo);
     OsExcRegInfo(excInfo);
+#if (LOSCFG_KERNEL_BACKTRACE == 1)
     OsExcBackTraceInfo(excInfo);
+#endif
     OsGetAllTskInfo();
     OsExcNvicDump();
     OsExcMemPoolCheckInfo();
--- arch/xtensa/lx6/gcc/los_interrupt.c
+++ arch/xtensa/lx6/gcc/los_interrupt.c
@@ -428,6 +428,7 @@ STATIC VOID OsExcRegInfo(const ExcInfo *excInfo)
     }
 }
 
+#if (LOSCFG_KERNEL_BACKTRACE == 1)
 STATIC VOID OsExcBackTraceInfo(const ExcInfo *excInfo)
 {
     UINTPTR LR[LOSCFG_BACKTRACE_DEPTH] = {0};
@@ -444,6 +445,7 @@ STATIC VOID OsExcBackTraceInfo(const ExcInfo *excInfo)
     }
     PRINTK("----- backtrace end -----\n");
 }
+#endif
 
 STATIC VOID OsExcMemPoolCheckInfo(VOID)
 {
@@ -493,7 +495,9 @@ STATIC VOID OsExcInfoDisplay(const ExcInfo *excInfo)
     OsExcTypeInfo(excInfo);
     OsExcCurTaskInfo(excInfo);
     OsExcRegInfo(excInfo);
+#if (LOSCFG_KERNEL_BACKTRACE == 1)
     OsExcBackTraceInfo(excInfo);
+#endif
     OsGetAllTskInfo();
     OsExcMemPoolCheckInfo();
 #endif
