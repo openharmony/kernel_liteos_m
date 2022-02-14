a1aeaf5d60dc0446677104c23b4bb111817a09cc
kenneth <zhushangyuan@huawei.com>
2022-02-14 20:14:40 +0800

    fix: los_interrupt.c存在未使用的参数编译出错
    修复los_interrupt.c存在未使用的参数编译出错
    
    close #I4SJPZ
    
    Signed-off-by: kenneth <zhushangyuan@huawei.com>

--- arch/arm/cortex-m4/iar/los_interrupt.c
+++ arch/arm/cortex-m4/iar/los_interrupt.c
@@ -209,11 +209,13 @@ LITE_OS_SEC_TEXT_MINOR VOID HalHwiDefaultHandler(VOID)
 
 WEAK VOID HalPreInterruptHandler(UINT32 arg)
 {
+    (VOID)arg;
     return;
 }
 
 WEAK VOID HalAftInterruptHandler(UINT32 arg)
 {
+    (VOID)arg;
     return;
 }
 
@@ -279,6 +281,7 @@ LITE_OS_SEC_TEXT_INIT UINT32 ArchHwiCreate(HWI_HANDLE_T hwiNum,
                                            HWI_PROC_FUNC handler,
                                            HWI_ARG_T arg)
 {
+    (VOID)mode;
     UINT32 intSave;
 
     if (handler == NULL) {
@@ -301,6 +304,7 @@ LITE_OS_SEC_TEXT_INIT UINT32 ArchHwiCreate(HWI_HANDLE_T hwiNum,
 #if (LOSCFG_PLATFORM_HWI_WITH_ARG == 1)
     OsSetVector(hwiNum, handler, arg);
 #else
+    (VOID)arg;
     OsSetVector(hwiNum, handler);
 #endif
     HwiUnmask((IRQn_Type)hwiNum);
