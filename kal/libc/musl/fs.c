ab46b9c8360b7c65aea423b4f46b7f394045e4b8
arvinzzz <zhaotianyu9@huawei.com>
2022-07-27 16:29:04 +0800

    feature: C库补充fs接口 close: #I5J5YD
    Signed-off-by: arvinzzz <zhaotianyu9@huawei.com>
    Change-Id: I21c11106b24b6e050d4061a98ad69b2b10a5a318

--- kal/libc/musl/fs.c
+++ kal/libc/musl/fs.c
@@ -28,22 +28,20 @@
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#define _GNU_SOURCE
 
 #include "los_config.h"
-#include "stdarg.h"
-#include "dirent.h"
-#include "sys/mount.h"
-#include "sys/statfs.h"
-#include "sys/stat.h"
-#include "unistd.h"
+#include <errno.h>
+#include <stdarg.h>
+#include <dirent.h>
+#include <sys/mount.h>
+#include <sys/statfs.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #ifdef LOSCFG_LIBC_MUSL_FS
 #include "los_fs.h"
-#else
-#include "sys/stat.h"
-#endif
 
-#ifdef LOSCFG_LIBC_MUSL_FS
 int mount(const char *source, const char *target,
           const char *filesystemtype, unsigned long mountflags,
           const void *data)
@@ -161,6 +159,23 @@ ssize_t pwrite(int fd, const void *buf, size_t nbyte, off_t offset)
     return LOS_Pwrite(fd, buf, nbyte, offset);
 }
 
+int access(const char *path, int mode)
+{
+    struct stat st;
+
+    if (stat(path, &st) < 0) {
+        return -1;
+    }
+    if ((st.st_mode & S_IFDIR) || (st.st_mode & S_IFREG)) {
+        return 0;
+    }
+    if ((mode & W_OK) && !(st.st_mode & S_IWRITE)) {
+        return -1;
+    }
+
+    return 0;
+}
+
 #else /* #ifdef LOSCFG_FS_VFS */
 
 int mount(const char *source, const char *target,
@@ -271,6 +286,11 @@ ssize_t pread(int fd, void *buf, size_t nbyte, off_t offset)
 }
 
 ssize_t pwrite(int fd, const void *buf, size_t nbyte, off_t offset)
+{
+    return -1;
+}
+
+int access(const char *path, int mode)
 {
     return -1;
 }
