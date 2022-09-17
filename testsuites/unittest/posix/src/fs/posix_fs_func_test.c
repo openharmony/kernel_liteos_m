0adbafb9c2032fb2a8d926eee97cba9610460c40
wangchen <253227059@qq.com>
2022-09-17 03:13:54 +0000

    \fix: M核用例编译问题修复 【背景】M核用例编译问题修复
    【修改方案】
    1, 添加musl缺失的fcntl
    2,删除musl下对fcntl和ioctl的依赖
    3,修改fs和vfs种fcntl入参的处理
    
    【影响】
    对现有的产品编译不会有影响。
    re #I5PKBJ
    Signed-off-by: wangchen <wangchen240@huawei.com>

--- testsuites/unittest/posix/src/fs/posix_fs_func_test.c
+++ testsuites/unittest/posix/src/fs/posix_fs_func_test.c
@@ -90,6 +90,7 @@ static BOOL PosixFsFuncTestSuiteTearDown(void)
     return TRUE;
 }
 
+#if (LOSCFG_LIBC_MUSL == 1)
 /* *
  * @tc.number   SUB_KERNEL_FS_DIRNAME_001
  * @tc.name     dirname basic function test
@@ -162,6 +163,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsDirname004, Function | MediumTest | L
     TEST_ASSERT_EQUAL_STRING(".", workDir);
     return 0;
 }
+#endif
 
 /* *
  * @tc.number   SUB_KERNEL_FS_FOPEN_FCLOSE_001
@@ -1443,7 +1445,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsUnlink001, Function | MediumTest | Le
     char tmpFileName[]= FILE1;
 
     fd = open(tmpFileName, O_CREAT | O_RDWR, 0777);
-    TEST_ASSERT_TRUE(ret != -1);
+    TEST_ASSERT_TRUE(fd != -1);
 
     (void)close(fd);
 
@@ -1454,7 +1456,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsUnlink001, Function | MediumTest | Le
 
 /* *
  * @tc.number   SUB_KERNEL_FS_STAT_001
- * @tc.name     unlink
+ * @tc.name     stat
  * @tc.desc     [C- SOFTWARE -0200]
  */
 LITE_TEST_CASE(PosixFsFuncTestSuite, testFsStat001, Function | MediumTest | Level1)
@@ -1466,7 +1468,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsStat001, Function | MediumTest | Leve
 
     remove(FILE1);
     fd = open(tmpFileName, O_CREAT | O_RDWR, 0777);
-    TEST_ASSERT_TRUE(ret != -1);
+    TEST_ASSERT_TRUE(fd != -1);
 
     (void)close(fd);
 
@@ -1477,7 +1479,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsStat001, Function | MediumTest | Leve
 
 /* *
  * @tc.number   SUB_KERNEL_FS_STAT_002
- * @tc.name     unlink
+ * @tc.name     stat
  * @tc.desc     [C- SOFTWARE -0200]
  */
 LITE_TEST_CASE(PosixFsFuncTestSuite, testFsStat002, Function | MediumTest | Level1)
@@ -1491,7 +1493,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsStat002, Function | MediumTest | Leve
 
     remove(FILE1);
     fd = open(tmpFileName, O_CREAT | O_RDWR, 0777);
-    TEST_ASSERT_TRUE(ret != -1);
+    TEST_ASSERT_TRUE(fd != -1);
 
     size = write(fd, writeBuf, sizeof(writeBuf));
     TEST_ASSERT_TRUE(ret != -1);
@@ -1507,7 +1509,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsStat002, Function | MediumTest | Leve
 
 /* *
  * @tc.number   SUB_KERNEL_FS_STAT_003
- * @tc.name     unlink
+ * @tc.name     stat
  * @tc.desc     [C- SOFTWARE -0200]
  */
 LITE_TEST_CASE(PosixFsFuncTestSuite, testFsStat003, Function | MediumTest | Level1)
@@ -1521,7 +1523,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsStat003, Function | MediumTest | Leve
 
     (void)memset_s(&buf, sizeof(buf), 0, sizeof(buf));
     fd = open(tmpFileName, O_CREAT | O_RDWR, 0777);
-    TEST_ASSERT_TRUE(ret != -1);
+    TEST_ASSERT_TRUE(fd != -1);
 
     size = write(fd, writeBuf, sizeof(writeBuf));
     TEST_ASSERT_TRUE(ret != -1);
@@ -1541,7 +1543,7 @@ extern off_t lseek(int fd, off_t offset, int whence);
 
 /* *
  * @tc.number   SUB_KERNEL_FS_WRITE_001
- * @tc.name     unlink
+ * @tc.name     write
  * @tc.desc     [C- SOFTWARE -0200]
  */
 LITE_TEST_CASE(PosixFsFuncTestSuite, testFsWrite001, Function | MediumTest | Level1)
@@ -1557,7 +1559,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsWrite001, Function | MediumTest | Lev
     }
 
     fd = open(tmpFileName, O_CREAT | O_RDWR, 0777);
-    TEST_ASSERT_TRUE(ret != -1);
+    TEST_ASSERT_TRUE(fd != -1);
 
     ret = write(fd, writeBuf, TEST_RW_SIZE);
     TEST_ASSERT_TRUE(ret != -1);
@@ -1577,7 +1579,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsWrite001, Function | MediumTest | Lev
 
 /* *
  * @tc.number   SUB_KERNEL_FS_WRITE_002
- * @tc.name     unlink
+ * @tc.name     write
  * @tc.desc     [C- SOFTWARE -0200]
  */
 LITE_TEST_CASE(PosixFsFuncTestSuite, testFsWrite002, Function | MediumTest | Level1)
@@ -1591,7 +1593,7 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsWrite002, Function | MediumTest | Lev
 
     remove(FILE1);
     fd = open(tmpFileName, O_CREAT | O_RDWR, 0777);
-    TEST_ASSERT_TRUE(ret != -1);
+    TEST_ASSERT_TRUE(fd != -1);
 
     for (i = 0; i < TEST_LOOPUP_TIME; i++) {
         ret = write(fd, writeBuf, sizeof(writeBuf));
@@ -1607,6 +1609,34 @@ LITE_TEST_CASE(PosixFsFuncTestSuite, testFsWrite002, Function | MediumTest | Lev
     return 0;
 }
 
+#if (LOSCFG_LIBC_MUSL == 1)
+/* *
+ * @tc.number   SUB_KERNEL_FS_FCNTL_001
+ * @tc.name     fcntl
+ * @tc.desc     [C- SOFTWARE -0200]
+ */
+LITE_TEST_CASE(PosixFsFuncTestSuite, testFsFcntl001, Function | MediumTest | Level1)
+{
+    int fd = 0;
+    int flags = 0;
+    char tmpFileName[]= FILE1;
+
+    fd = open(tmpFileName, O_CREAT | O_RDWR, 0777);
+    TEST_ASSERT_TRUE(fd != -1);
+
+    flags = fcntl(fd, F_GETFL);
+    TEST_ASSERT_TRUE(flags == O_CREAT | O_RDWR);
+
+    fcntl(fd, F_SETFL, flags | O_APPEND);
+    flags = fcntl(fd, F_GETFL);
+    TEST_ASSERT_TRUE(flags == O_CREAT | O_RDWR | O_APPEND);
+
+    (void)close(fd);
+
+    return 0;
+}
+#endif
+
 RUN_TEST_SUITE(PosixFsFuncTestSuite);
 
 void PosixFsFuncTest()
@@ -1621,12 +1651,13 @@ void PosixFsFuncTest()
 
     RUN_ONE_TESTCASE(testFsWrite001);
     RUN_ONE_TESTCASE(testFsWrite002);
-
+#if (LOSCFG_LIBC_MUSL == 1)
     RUN_ONE_TESTCASE(testFsDirname001);
     RUN_ONE_TESTCASE(testFsDirname002);
     RUN_ONE_TESTCASE(testFsDirname003);
     RUN_ONE_TESTCASE(testFsDirname004);
-
+    RUN_ONE_TESTCASE(testFsFcntl001);
+#endif
     RUN_ONE_TESTCASE(testFsReaddir001);
     RUN_ONE_TESTCASE(testFsReaddir002);
 
@@ -1688,6 +1719,5 @@ void PosixFsFuncTest()
     RUN_ONE_TESTCASE(testFsFreadFwrite007);
     RUN_ONE_TESTCASE(testFsFreadFwrite008);
     RUN_ONE_TESTCASE(testFsFreadFwrite009);
-
     return;
 }
