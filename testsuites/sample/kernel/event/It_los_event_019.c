d14a45a1c55803858901350dbdcee3eae659e3c1
liuwenxin <liuwenxin11@huawei.com>
2022-11-10 14:55:29 +0800

    Fix:m核告警清理
    Signed-off-by: liuwenxin <liuwenxin11@huawei.com>

--- testsuites/sample/kernel/event/It_los_event_019.c
+++ testsuites/sample/kernel/event/It_los_event_019.c
@@ -90,7 +90,7 @@ static UINT32 Testcase(VOID)
     LOS_EventInit(&g_pevent);
 
     ret = LOS_TaskCreate(&g_testTaskID01, &task1);
-    ICUNIT_GOTO_EQUAL(ret, LOS_OK, ret, EXIT);
+    ICUNIT_ASSERT_EQUAL(ret, LOS_OK, ret);
 
     ICUNIT_ASSERT_EQUAL(g_testCount, 1, g_testCount);
     g_testCount++;
@@ -111,7 +111,6 @@ static UINT32 Testcase(VOID)
 EXIT1:
     LOS_TaskDelete(g_testTaskID02);
 
-EXIT:
     LOS_TaskDelete(g_testTaskID01);
 
     return LOS_OK;
