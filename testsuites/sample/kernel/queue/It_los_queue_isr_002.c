1b778c4267d2fba9f24468b38a58146f772bd99a
zengwenlong <zengwenlong@openvalley.net>
2023-09-11 20:47:44 -0700

    feat: 增加内核queue在中断中进行读写操作的接口
    方案描述:
    1,基于 OsQueueOperate增加支持queue在中断中进行读写操作的接口。
    BREAKING CHANGE:
    增加支持queue在中断中进行读写操作的接口，需要适配接口描述：
    适配API:
    
    新增API：
    UINT32 LOS_QueueReadCopyIsr(UINT32 queueID,   VOID *bufferAddr, UINT32 *bufferSize);
    UINT32 LOS_QueueWriteCopyIsr(UINT32 queueID, VOID *bufferAddr, UINT32 bufferSize);
    UINT32 LOS_QueueReadIsr(UINT32 queueID,VOID *bufferAddr,  UINT32 bufferSize);
    UINT32 LOS_QueueWriteIsr(UINT32 queueID, VOID *bufferAddr,  UINT32 bufferSize);
    UINT32 LOS_QueueWriteHeadIsr(UINT32 queueID, VOID *bufferAddr,UINT32 bufferSize);
    UINT32 LOS_QueueWriteHeadCopyIsr(UINT32 queueID,  VOID *bufferAddr,UINT32 bufferSize);
    
    Signed-off-by: zengwenlong <zengwenlong@openvalley.net>

---
+++ testsuites/sample/kernel/queue/It_los_queue_isr_002.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2023 Hunan OpenValley Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "It_los_queue.h"
+
+
+static VOID HwiF01(VOID)
+{
+    UINT32 ret;
+    CHAR buff1[QUEUE_SHORT_BUFFER_LENGTH] = "UniDSP";
+
+    ret = LOS_QueueWriteIsr(g_testQueueID01, &buff1, QUEUE_BASE_MSGSIZE);
+    ICUNIT_ASSERT_EQUAL_VOID(ret, LOS_ERRNO_QUEUE_NOT_CREATE, ret);
+}
+
+static UINT32 Testcase(VOID)
+{
+    UINT32 ret;
+    HWI_PRIOR_T hwiPrio = 3;
+    HWI_MODE_T mode = 0;
+    CHAR *buff2 = NULL;
+    HwiIrqParam irqParam;
+
+    ret = LOS_QueueCreate("Q1", QUEUE_BASE_NUM, &g_testQueueID01, 0, 0);
+    ICUNIT_GOTO_EQUAL(ret, LOS_ERRNO_QUEUE_PARA_ISZERO, ret, EXIT);
+    (VOID)memset_s(&irqParam, sizeof(HwiIrqParam), 0, sizeof(HwiIrqParam));
+    irqParam.pDevId = 0;
+    ret = LOS_HwiCreate(HWI_NUM_TEST, hwiPrio, mode, (HWI_PROC_FUNC)HwiF01, &irqParam);
+    ICUNIT_ASSERT_EQUAL(ret, LOS_OK, ret);
+
+    TestHwiTrigger(HWI_NUM_TEST);
+
+    ret = LOS_QueueRead(g_testQueueID01, &buff2, QUEUE_BASE_MSGSIZE, 0);
+    ICUNIT_GOTO_EQUAL(ret, LOS_ERRNO_QUEUE_NOT_CREATE, ret, EXIT);
+
+EXIT:
+    TestHwiDelete(HWI_NUM_TEST);
+    ret = LOS_QueueDelete(g_testQueueID01);
+    ICUNIT_ASSERT_EQUAL(ret, LOS_ERRNO_QUEUE_NOT_CREATE, ret);
+    return LOS_OK;
+}
+
+VOID ItLosQueueIsr002(VOID)
+{
+    TEST_ADD_CASE("ItLosQueueIsr002", Testcase, TEST_LOS, TEST_QUE, TEST_LEVEL1, TEST_FUNCTION);
+}
+
